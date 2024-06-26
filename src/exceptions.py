5e38c99fba224140ad2a0bf34695f41ef5d90157
BharatSingh Rajpurohit <bsraigur@gmail.com>
2024-06-26 22:25:00 +0530

    logs making error solved

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.loggers import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -19,10 +19,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-# if __name__== "__main__":
+if __name__== "__main__":
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divided by Zero")
-#         raise CustomException(e,sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by Zero")
+        raise CustomException(e,sys)
--- src/loggers.py
+++ src/loggers.py
@@ -16,5 +16,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# if __name__== "__main__":
-#     logging.info("Logging has started")
+if __name__== "__main__":
+    logging.info("Logging has started")
